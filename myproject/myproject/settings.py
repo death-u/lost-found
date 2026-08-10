8b2a28d9d2720ca3f5452fa27a67c315e973475d
death-u <ngadisuccess01@gmail.com>
2026-08-10 10:53:18 +0100

    made some changes and prepared for first upload

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-59!87x_wdy34x-e#e58sfup9yz!jq!a7%m*+8w_kmd_r-zx=^&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
