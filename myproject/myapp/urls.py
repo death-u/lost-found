dc73d6ec025d135cde7af21ddf67fdfb606e32d8
death-u <ngadisuccess01@gmail.com>
2026-08-10 10:40:55 +0100

    just added more files and finshed the index.html page bith button functions

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name='index'),
+    path('found_items/',views.found, name='found_items'),
+    path('lost_items/',views.lost, name='lost_items'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+# basic function to render the index.html template
 def index(request):
     return render(request, 'files/index.html');
+
+# found logic to render the found_items.html template
+def found(request):
+    return render(request, 'files/found_items.html');
+
+def lost(request):
+    return render(request,'files/lost_items.html');
